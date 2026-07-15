e914eef81cf5a8c651b92a095c0967449e36eaed
pxrtth-bxnsxl <parthbansal937@gmail.com>
2026-07-15 23:28:31 +0530

    Time: 0 ms (100%), Space: 45 MB (5.33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,35 +2,36 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
         int idx = -1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
+        
+        // 1. Find the first decreasing element from the end
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
                 idx = i;
                 break;
             }
         }
-        if(idx==-1){
-            Arrays.sort(nums);
-        }
-        if(idx>=0){
-        for(int i=n-1;i>idx;i--){
-            if(nums[i]>nums[idx]){
-                int temp = nums[i];
-                nums[i] = nums[idx];
-                nums[idx] = temp;
-                break;
+        
+        // 2. If we found a valid dip, find the next largest element and swap
+        if(idx >= 0) {
+            for(int i = n - 1; i > idx; i--) {
+                if(nums[i] > nums[idx]) {
+                    int temp = nums[i];
+                    nums[i] = nums[idx];
+                    nums[idx] = temp;
+                    break;
+                }
             }
         }
-        int left = idx+1;
-        int right = n-1;
-        while(left<right){
+        
+        // 3. Reverse the suffix (or the whole array if idx == -1)
+        int left = idx + 1;
+        int right = n - 1;
+        while(left < right) {
             int temp = nums[left];
             nums[left] = nums[right];
             nums[right] = temp;
             left++;
             right--;
         }
-        }
-    
-        
     }
 }
