ae982cd6e250af2b4d7eab7e86aac12ac4b5c27f
pxrtth-bxnsxl <parthbansal937@gmail.com>
2026-07-19 01:18:05 +0530

    Time: 0 ms (100%), Space: 44 MB (29.33%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -4,7 +4,7 @@ class Solution {
         int j = n-1;
         int k = m+n-1;
         while(i>=0&&j>=0){
-            if(nums1[i]>nums2[j]){
+            if(nums1[i]>=nums2[j]){
                 nums1[k--] = nums1[i--]; 
             }
             else{
