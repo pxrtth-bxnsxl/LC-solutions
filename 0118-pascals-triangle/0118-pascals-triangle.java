0fe72edc7ef4fac68f989c56b4029914f0ca5f5f
pxrtth-bxnsxl <parthbansal937@gmail.com>
2026-07-26 11:15:56 +0530

    Time: 1 ms (94.32%), Space: 43 MB (98.4%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -3,10 +3,10 @@ class Solution {
         List<List<Integer>> list = new ArrayList<>();
         for(int i=0;i<numRows;i++){
             List<Integer> root = new ArrayList<>();
-            int val = 1;
-            for(int j=0;j<=i;j++){
-                root.add(val);
-                val= val*(i-j)/(j+1);
+            int ans = 1;
+            for(int j =0;j<=i;j++){
+                root.add(ans);
+                ans = ans*(i-j)/(j+1);
             }
             list.add(root);
         }
